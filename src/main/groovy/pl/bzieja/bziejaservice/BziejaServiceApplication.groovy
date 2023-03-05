c0d16d2a0dd461d9ce1a8018bcd43c112eea747c
bzieja <56655333+bzieja@users.noreply.github.com>
2023-03-05 20:53:59 +0100

    Initial commit

---
+++ src/main/groovy/pl/bzieja/bziejaservice/BziejaServiceApplication.groovy
@@ -0,0 +1,13 @@
+package pl.bzieja.bziejaservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BziejaServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BziejaServiceApplication, args)
+    }
+
+}
