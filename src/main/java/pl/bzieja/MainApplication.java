900e9705dce2287a2ecfa5faa6897446c3c6fc2b
bzieja <56655333+bzieja@users.noreply.github.com>
2023-03-06 23:35:19 +0100

    add java service

---
+++ src/main/java/pl/bzieja/MainApplication.java
@@ -0,0 +1,17 @@
+package pl.bzieja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bartłomiej Zieja
+ */
+@SpringBootApplication
+class MainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+
+  }
+
+}
